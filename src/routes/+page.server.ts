8e69317f7aa6da12deb7726ffa06857471bd5bc1
Kirsten Corrao <kirstencorrao@gmail.com>
2023-09-24 20:05:32 -0700

    gets n closest campgrounds/facilities

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,16 @@
 /** @type {import('./$types').PageServerLoad} */
 ///** @type {import('./$types').PageLoad} */
 export async function load( {fetch}) {
-  console.log('fetching rec areas....')
-	const response = await fetch('/api/recreation_areas?id=13326', {
+  console.log('fetching 10 closest facilities....')
+	const lat = '44.30194037928479';
+	const long = '-120.85251454882285';
+	const response = await fetch(`/api/facilities?lat=${lat}&long=${long}&limit=5`, {
 		method: 'GET',
 		headers: {
 			'content-type': 'application/json'
 		}
 	});
 
-	const areas = await response.json();
-	return areas;
+	const facilities = await response.json();
+	return facilities;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-{#each data.data as row }
-  <p>Rec area is {row.name}</p>
-	<p>{@html row.description}</p>
-{/each}
+{#if data.data}
+	<ol>
+		{#each data.data as row }
+		<li>{ row.name } ({ row.type }): { row.distance_miles } miles away</li>
+		{/each}
+	</ol>
+{/if}
